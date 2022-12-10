a163d1f591489e086ca46b0b8ae4335e752a51f2
Shacu <nicolasdoga62@gmail.com>
2022-12-09 21:42:20 -0300

    con error

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,21 +1,25 @@
 import { useEffect, useState } from "react";
+import '/Items.json';
 
 const ItemListContainer = (props) => {
     const [items, setItems] = useState([])
 
-useEffect(() => {
-    fetch("./Items/Items.js")
-    .then(Response => Response.json())
-    .then(item => {
-        setItems(item)
-    })
-},[])
+    useEffect(() => {
+        fetch("/Items.json")
+        .then(Response => Response.json())
+        .then(item => {
+            setItems(item)
+        })
+    },[])
 
 
     return (
         <>
         <p>{props.src}</p>
         <div>
+            {items.map((arrItems) => (
+                <div>{arrItems.nombre}</div>
+            ))}
         <img src="" alt="Card image cap"></img>
             <div>
                 <h5>Card title</h5>
